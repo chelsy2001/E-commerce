305d2d9db829bc4d31746ed837140f42d54b2f19
chelsy2 <chelsy.yadav@badho.in>
2024-05-05 02:40:41 +0530

    new collectin screen

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offer from '../Components/Offer/Offer'
+import NewCollections from '../Components/New collections/NewCollections'
 
 function Shop() {
   return (
@@ -9,6 +10,7 @@ function Shop() {
       <Hero />
       <Popular />
       <Offer />
+      <NewCollections />
     </div>
   )
 }

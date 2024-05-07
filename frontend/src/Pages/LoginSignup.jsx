9b761faf951d7f829eb545a416c781dc1ebf3bd0
chelsy2001 <chelsyyadav22@gmail.com>
2024-05-08 01:26:16 +0530

    add shop category screen contains

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function LoginSignup() {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className='loginsignup-container'>
+        <h1>
+          Sign up
+        </h1>
+        <div className='loginsignup-fields'>
+          <input type='text' placeholder='Your Name'></input>
+          <input type='email' placeholder='Email Address'></input>
+          <input type='password' placeholder='Password'></input>
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account</p>
+        <div className='loginsignup-agree'>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offer from '../Components/Offer/Offer'
 import NewCollections from '../Components/New collections/NewCollections'
+import NewsLetter from '../Components/Newsletter/NewsLetter'
 
 function Shop() {
   return (
@@ -11,6 +12,7 @@ function Shop() {
       <Popular />
       <Offer />
       <NewCollections />
+      <NewsLetter />
     </div>
   )
 }

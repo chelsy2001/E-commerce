80c91f197fcd5b18cd1ae648f46e10c19cf37297
chelsy2001 <chelsyyadav22@gmail.com>
2024-05-19 15:48:07 +0530

    setup admin panel

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ListProduct.css'
+
+const ListProduct = () => {
+  return (
+    <div className='list-product'>
+      <P>LIST OF PRODUCTS </P>
+    </div>
+  )
+}
+
+export default ListProduct
